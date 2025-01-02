f83cbe5ecb9064e0c2d5160d27cc32e06d8ed89b
Leandro Matus <leandromatus85@gmail.com>
2025-01-02 15:42:23 -0600

    feat: enhance error handling in ProductsService by including HTTP status in RpcException

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,5 @@
 import { PaginationDto } from './../common/dto/pagination.dto';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -43,7 +43,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product) {
-      throw new RpcException(`Product with id ${id} not found`);
+      throw new RpcException({
+        message: `Product with id ${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
     }
     return product;
   }
