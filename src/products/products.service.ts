d54cc67db86f64ccbde6a9fc9bbc4450fb644355
Leandro Matus <leandromatus85@gmail.com>
2025-01-02 11:18:54 -0600

    feat: initialize NestJS application with product management module and basic configurations

---
+++ src/products/products.service.ts
@@ -0,0 +1,66 @@
+import { PaginationDto } from './../common/dto/pagination.dto';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    console.log('Connected to the database');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({ data: createProductDto });
+  }
+
+  async findAll(PaginationDto: PaginationDto) {
+    const { page, limit } = PaginationDto;
+
+    const totalPages = await this.product.count();
+
+    const lastPage = Math.ceil(totalPages / limit);
+    return {
+      data: this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      meta: {
+        page,
+        total: totalPages,
+        lastPage,
+      },
+    };
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findFirst({ where: { id } });
+
+    if (!product) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+
+    return this.product.update({
+      data: updateProductDto,
+      where: { id },
+    });
+  }
+
+  async remove(id: number) {
+    await this.findOne(id);
+
+    return this.product.delete({ where: { id } });
+  }
+}
